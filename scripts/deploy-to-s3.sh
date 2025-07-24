ddfe267dfe4bd052e7c0b6812b82a2424ece39d8
AlokTiwari-spec <AlokTiwari-spec@users.noreply.github.com>
2025-07-24 15:27:06 +0530

    Updated deploy.yml

--- scripts/deploy-to-s3.sh
+++ scripts/deploy-to-s3.sh
@@ -8,25 +8,29 @@ fi
 
 BUILD_PATH="dist/login-app"
 
-# Get the latest version number
-LATEST_VERSION=$(aws s3 ls s3://$AWS_S3_BUCKET/ | grep -v index.html | grep -o '^.*v[0-9]\+/' | grep -o '[0-9]\+' | sort -n | tail -n 1)
-if [ -z "$LATEST_VERSION" ]; then
-    NEW_VERSION=1
-else
-    NEW_VERSION=$((LATEST_VERSION + 1))
-fi
-
-# Upload the new version
-aws s3 cp $BUILD_PATH/ s3://$AWS_S3_BUCKET/v$NEW_VERSION/ --recursive
+# First try to upload to a temporary location to verify the upload works
+aws s3 cp $BUILD_PATH/ s3://$AWS_S3_BUCKET/temp/ --recursive
 UPLOAD_STATUS=$?
 
-# Check if the upload was successful
-if [ $UPLOAD_STATUS -eq 1 ]; then
-    # Update the latest version
-    aws s3 cp $BUILD_PATH/ s3://$AWS_S3_BUCKET/ --recursive
+if [ $UPLOAD_STATUS -eq 0 ]; then
+    # Upload was successful, now get the latest version number
+    LATEST_VERSION=$(aws s3 ls s3://$AWS_S3_BUCKET/ | grep -v index.html | grep -o '^.*v[0-9]\+/' | grep -o '[0-9]\+' | sort -n | tail -n 1)
+    if [ -z "$LATEST_VERSION" ]; then
+        NEW_VERSION=1
+    else
+        NEW_VERSION=$((LATEST_VERSION + 1))
+    fi
+
+    # Move from temp to versioned folder
+    aws s3 mv s3://$AWS_S3_BUCKET/temp/ s3://$AWS_S3_BUCKET/v$NEW_VERSION/ --recursive
+    # Update the root (latest version)
+    aws s3 cp s3://$AWS_S3_BUCKET/v$NEW_VERSION/ s3://$AWS_S3_BUCKET/ --recursive
+    # Clean up temp folder if anything remains
+    aws s3 rm s3://$AWS_S3_BUCKET/temp/ --recursive
     echo "Successfully deployed version v$NEW_VERSION"
 else
     echo "Upload failed with status code: $UPLOAD_STATUS"
+    # Clean up temp folder if anything was uploaded
     # Get the previous version
     PREVIOUS_VERSION=$(aws s3 ls s3://$AWS_S3_BUCKET/ | grep -v index.html | grep -o '^.*v[0-9]\+/' | grep -o '[0-9]\+' | sort -n | tail -n 1)
     if [ ! -z "$PREVIOUS_VERSION" ]; then
